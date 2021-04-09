7628215aba8bd9393b0bcdd3e52b059767506be2
Léopold <leopoq@gmail.com>
2021-04-09 17:35:16 +0200

    Que nous testassions

--- solv_func.py
+++ solv_func.py
@@ -37,6 +37,30 @@ class SolvFunc:
 
 
     def candidat_bloque(self,c):  # https://www.sudoku129.com/grilles/tips_2.php
-        col, lig, blc = c.colonne, c.ligne, c.bloc
+        col, lig, blc = c.colonne_ap, c.ligne_ap, c.bloc_ap
+        col_g = self.g.collone(col)
+        lig_g = self.g.ligne(lig)
         for p in c.possibilites:
-            self.g
+            flag = True
+            C=[]
+            for cases in col_g:
+                if p in cases.possibilites and cases != c:
+                    C.append(cases)  # les cases qui ont aussi cette possibilité
+            b=[]
+            for cases in C:
+                if cases.bloc_ap not in B:
+                    b.append(cases.bloc_ap)
+            if len(b)==1:
+                if b[0] != blc:
+                    B=self.bloc(b[0])
+                    cases_autorisees=[]
+                    for case in B:
+                        if p in case.possibilites:
+                            cases_autorisees.append(case)
+                    for case in cases_autorisees:
+                        if case.colonne_ap != col_g:
+                            flag=False
+            if flag:
+                c.possibilites.remove(p)
+
+
